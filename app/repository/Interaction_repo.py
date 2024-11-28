1df7250a19d15a89a4c814a0c53f7e5fe715f411
chaim <shterenchaim@gmail.com>
2024-11-28 16:21:07 +0200

    create rest of endpoints

--- app/repository/Interaction_repo.py
+++ app/repository/Interaction_repo.py
@@ -1,6 +1,7 @@
 from returns.maybe import Maybe
 from app.db.database import driver
 from app.db.models import Interaction
+import toolz as t
 
 
 def create_interaction_repo(interaction: Interaction):
@@ -28,3 +29,67 @@ def create_interaction_repo(interaction: Interaction):
         res = session.run(query, params).single()
         return (Maybe.from_optional(res.get("d"))
                 .map(lambda d: dict(d)))
+
+
+def get_connection_by_method_repo(method):
+    with driver.session() as session:
+        query = """
+        match path =(d:Device) -[:CONNECTED{method: $method}]->(d1:Device) where not d = d1 return d,d1,length(path) as len
+        """
+        params = {
+            "method": method
+        }
+        res = session.run(query, params).data()
+        return res
+
+
+def get_connection_stronger_then_repo(streng_num: int):
+    with driver.session() as session:
+        query = """
+        match(d:Device) -[rel:CONNECTED]-> (d1:Device) where rel.signal_strength_dbm >=$streng_num return d,d1
+        """
+        params = {
+            "streng_num": streng_num
+        }
+        res = session.run(query, params).data()
+        return res
+
+
+def get_sum_connections_to_repo(device_id):
+    with driver.session() as session:
+        query = """
+        match(d:Device{id:$device_id}) <-- (d1:Device) 
+        return count(d) as sum_connection
+        """
+        params = {
+            "device_id": device_id
+        }
+        res = session.run(query, params).data()
+        return res
+
+
+def check_if_too_connected_repo(devices):
+    with driver.session() as session:
+        query = """
+        match(d:Device{id: $from_device }) -[:CONNECTED]-> (d1:Device{id: $to_device })  return d,d1
+        """
+        params = {
+            "from_device": devices["from_device"],
+            "to_device": devices["to_device"]
+        }
+
+        res = session.run(query, params).single()
+        return True if res else False
+
+
+def get_last_connection_repo(device_id):
+    with driver.session() as session:
+        query = """
+        match(d:Device{id: $device_id}) -[rel:CONNECTED]-> (d1:Device) 
+        return d,d1 order by rel.timestamp  asc limit 1
+        """
+        params = {
+            "device_id": device_id
+        }
+        res = session.run(query, params).data()
+        return res
--- app/rout/phone_rout.py
+++ app/rout/phone_rout.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
-
-from app.db.models import Device, Location
-from app.repository.Interaction_repo import create_interaction_repo
+from app.repository.Interaction_repo import create_interaction_repo, get_connection_by_method_repo, \
+    get_connection_stronger_then_repo, get_sum_connections_to_repo, check_if_too_connected_repo
 from app.repository.device_repo import create_device_repo
 from app.service.split_data import split_data_to_models
 
@@ -17,3 +16,57 @@ def get_interaction():
     create_interaction_repo(split_data["interaction"])
     print(request.json)
     return jsonify({}), 200
+
+
+@phone_blueprint.route("/connection/<method>", methods=['GET'])
+def get_connection_by_method(method):
+    res = get_connection_by_method_repo(method)
+    try:
+        return jsonify({"res": res}), 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({}), 404
+
+
+@phone_blueprint.route("/relation_stronger_then/<int: streng_num>", methods=['GET'])
+def get_connection_stronger_then(streng_num):
+    res = get_connection_stronger_then_repo(streng_num)
+    try:
+        return jsonify({"res": res}), 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({}), 404
+
+
+@phone_blueprint.route("/all_connected_to/<device_id>", methods=['GET'])
+def get_sum_connections_to(device_id):
+    res = get_sum_connections_to_repo(device_id)
+    try:
+        return jsonify({"res": res}), 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({}), 404
+
+
+@phone_blueprint.route("/if_connect", methods=['POST'])
+def check_if_too_connected():
+    bool_res = check_if_too_connected_repo(request.json)
+    if bool_res:
+        res = "found connection"
+    else:
+        res = "not found connection"
+    try:
+        return jsonify({"res": res}), 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({}), 404
+
+
+@phone_blueprint.route("/last_connection/<device_id>", methods=['GET'])
+def get_last_connection(device_id):
+    res = get_last_connection_repo(device_id)
+    try:
+        return jsonify({"res": res}), 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({}), 404
